2b0911a55eb7243f47a42bfe74737b3337256147
TheSicilian12 <mguidera314@gmail.com>
2023-04-19 20:14:07 -0400

    working on data format for karuta cards

---
+++ Data/KarutaCardData.js
@@ -0,0 +1,43 @@
+cards = {
+    0: {
+        'english': {
+            'yomifuda':{
+                'firstLine': 'Coarse the rush-mat roof',
+                'secondLine': 'Sheltering the harvest-hut',
+                'thirdLine': 'Of the autumn rice-field;',
+            },
+            'torifuda': {
+                'firstLine': 'And my sleeves are growing wet',
+                'secondLine': 'With the moisture dripping through.'
+            }
+        },
+        'japanese': {
+            'yomifuda':{
+                'firstLine': '秋の田の',
+                'secondLine': 'かりほの庵の',
+                'thirdLine': '苫をあらみ',
+            },
+            'torifuda': {
+                'firstLine': 'わが衣手は',
+                'secondLine': '露にぬれつつ'
+            }
+        },
+        'romaji': {
+            'yomifuda':{
+                'firstLine': 'Aki no ta no',
+                'secondLine': 'Kariho no io no',
+                'thirdLine': 'Toma o arami',
+            },
+            'torifuda': {
+                'firstLine': 'Waga koromode wa',
+                'secondLine': 'Tsuyu ni nure tsutsu'
+            }
+        },
+        cardData: {
+
+        }
+    },
+    citations: {
+        'webSourceUrl': 'https://jti.lib.virginia.edu/japanese/hyakunin/index.html'
+    }
+}
