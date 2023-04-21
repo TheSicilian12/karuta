b0482cdf9595364e9226a310ee4b9b56b15e5083
TheSicilian12 <mguidera314@gmail.com>
2023-04-20 20:12:46 -0400

    card data structure

--- Data/KarutaCardData.js
+++ Data/KarutaCardData.js
@@ -1,75 +1,35 @@
 cards = {
     0: {
-
-        'yomifuda': {
-            'english': {
-                'firstLine': 'Coarse the rush-mat roof',
-                'secondLine': 'Sheltering the harvest-hut',
-                'thirdLine': 'Of the autumn rice-field;',
-            },
-            'japanese':{
-                'firstLine': '秋の田の',
-                'secondLine': 'かりほの庵の',
-                'thirdLine': '苫をあらみ',
-            }
+        'author': {
+            'english': 'Emperor Tenchi',
+            'japanese': '天智天皇',
+            'romaji': 'Tenchi Tenno'
         },
-        'torifuda': {
-            'english': {
-                'firstLine': 'And my sleeves are growing wet',
-                'secondLine': 'With the moisture dripping through.'
-            },
-            'japanese': {
-                'firstLine': 'わが衣手は',
-                'secondLine': '露にぬれつつ'
-            }
-        },
-
-
         'english': {
-            'yomifuda':{
-                'firstLine': 'Coarse the rush-mat roof',
-                'secondLine': 'Sheltering the harvest-hut',
-                'thirdLine': 'Of the autumn rice-field;',
-            },
-            'torifuda': {
-                'firstLine': 'And my sleeves are growing wet',
-                'secondLine': 'With the moisture dripping through.'
-            }
+            0: 'Coarse the rush-mat roof',
+            1: 'Sheltering the harvest-hut',
+            2: 'Of the autumn rice-field;',
+            3: 'And my sleeves are growing wet',
+            4: 'With the moisture dripping through.'
         },
         'japanese': {
-            'yomifuda':{
-                'firstLine': '秋の田の',
-                'secondLine': 'かりほの庵の',
-                'thirdLine': '苫をあらみ',
-            },
-            'torifuda': {
-                'firstLine': 'わが衣手は',
-                'secondLine': '露にぬれつつ'
-            }
+            0: '秋の田の',
+            1: 'かりほの庵の',
+            2: '苫をあらみ',
+            3: 'わが衣手は',
+            4: '露にぬれつつ'
         },
         'romaji': {
-            'yomifuda':{
-                'firstLine': 'Aki no ta no',
-                'secondLine': 'Kariho no io no',
-                'thirdLine': 'Toma o arami',
-            },
-            'torifuda': {
-                'firstLine': 'Waga koromode wa',
-                'secondLine': 'Tsuyu ni nure tsutsu'
-            }
+            0: 'Aki no ta no',
+            1: 'Kariho no io no',
+            2: 'Toma o arami',
+            3: 'Waga koromode wa',
+            4: 'Tsuyu ni nure tsutsu'
         },
-        'cardData': {
-            'author': {
-                'english': 'Emperor Tenchi',
-                'japanese': '天智天皇',
-                'romaji': 'Tenchi Tenno'
-            }
-        }
     },
     'citations': {
         'webSourceUrl': 'https://jti.lib.virginia.edu/japanese/hyakunin/index.html',
         'organization': 'The University of Virginia Library Electronic Text Center and the University of Pittsburgh East Asian Library'
     }
 }
-// currently language -> yomifuda -> text
-// think about adjusting structure to be yomifuda -> language -> language text
+
