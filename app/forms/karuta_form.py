34086c0777feb25f16ac4e2b29d8da378efed5d3
TheSicilian12 <mguidera314@gmail.com>
2023-07-06 13:12:14 -0400

    karuta form

--- app/forms/karuta_form.py
+++ app/forms/karuta_form.py
@@ -20,5 +20,43 @@ class karutaForm(FlaskForm):
         'english_line5', validators=[DataRequired()]
         )
     english_author = StringField(
-        'english_line6', validators=[DataRequired()]
+        'english_author', validators=[DataRequired()]
+        )
+
+    japanese_line1 = StringField(
+        'japanese_line1', validators=[DataRequired()]
+        )
+    japanese_line2 = StringField(
+        'japanese_line2', validators=[DataRequired()]
+        )
+    japanese_line3 = StringField(
+        'japanese_line3', validators=[DataRequired()]
+        )
+    japanese_line4 = StringField(
+        'japanese_line4', validators=[DataRequired()]
+        )
+    japanese_line5 = StringField(
+        'japanese_line5', validators=[DataRequired()]
+        )
+    japanese_author = StringField(
+        'japanese_author', validators=[DataRequired()]
+        )
+
+    romaji_line1 = StringField(
+        'romaji_line1', validators=[DataRequired()]
+        )
+    romaji_line2 = StringField(
+        'romaji_line2', validators=[DataRequired()]
+        )
+    romaji_line3 = StringField(
+        'romaji_line3', validators=[DataRequired()]
+        )
+    romaji_line4 = StringField(
+        'romaji_line4', validators=[DataRequired()]
+        )
+    romaji_line5 = StringField(
+        'romaji_line5', validators=[DataRequired()]
+        )
+    romaji_author = StringField(
+        'romaji_author', validators=[DataRequired()]
         )
