7d163767b26842e8c47441b00795ef928548ab02
TheSicilian12 <mguidera314@gmail.com>
2023-07-06 12:47:57 -0400

    getting home page connected

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
+import HomePage from "./components/HomePage";
 
 function App() {
   const dispatch = useDispatch();
@@ -20,7 +19,7 @@ function App() {
       {isLoaded && (
         <Switch>
           <Route path="/" exact>
-            <Navigation isLoaded={isLoaded} />
+            <HomePage />
           </Route>
         </Switch>
       )}
