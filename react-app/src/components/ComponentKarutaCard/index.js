cc5ab3849d9011f967e236db14eecaea5ce3868a
TheSicilian12 <mguidera314@gmail.com>
2023-07-07 11:42:50 -0400

    working on card component

---
+++ react-app/src/components/ComponentKarutaCard/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+
+import "./ComponentKarutaCard.css";
+
+export default function ComponentKarutaCard(cardData) {
+
+  return (
+   <div className='karuta-card-container'>
+    
+
+    </div>
+  );
+}
--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { getAllKarutaCardsTHUNK } from "../../store/karutaCards";
 
 import "./HomePage.css";
+import ComponentKarutaCard from "../ComponentKarutaCard";
 
 export default function HomePage() {
   const dispatch = useDispatch();
+
+  const cards = useSelector(state => state.karutaCards)
 
   useEffect(() => {
     dispatch(getAllKarutaCardsTHUNK())
@@ -14,6 +17,9 @@ export default function HomePage() {
 
 
   return (
-   <div>Hello</div>
+   <div>
+    Hello
+    <ComponentKarutaCard cardData={cards[0]}/>
+    </div>
   );
 }
