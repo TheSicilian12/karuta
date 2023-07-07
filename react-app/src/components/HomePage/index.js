4db064642a7413142577119c1ae2892f69c38b2b
TheSicilian12 <mguidera314@gmail.com>
2023-07-07 10:35:05 -0400

    working on api call for cards

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from 'react-redux';
 
 import "./HomePage.css";
 
 export default function HomePage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    
+  }, dispatch)
+
 
   return (
    <div>Hello</div>
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,100 +1,13 @@
 // constants
-const SET_USER = "session/SET_USER";
-const REMOVE_USER = "session/REMOVE_USER";
+const LOAD_KARUTA_CARDS = 'cards/all'
 
-const setUser = (user) => ({
-	type: SET_USER,
-	payload: user,
-});
 
-const removeUser = () => ({
-	type: REMOVE_USER,
-});
 
-const initialState = { user: null };
 
-export const authenticate = () => async (dispatch) => {
-	const response = await fetch("/api/auth/", {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
-	if (response.ok) {
-		const data = await response.json();
-		if (data.errors) {
-			return;
-		}
 
-		dispatch(setUser(data));
-	}
-};
+const initialState = {};
 
-export const login = (email, password) => async (dispatch) => {
-	const response = await fetch("/api/auth/login", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			email,
-			password,
-		}),
-	});
-
-	if (response.ok) {
-		const data = await response.json();
-		dispatch(setUser(data));
-		return null;
-	} else if (response.status < 500) {
-		const data = await response.json();
-		if (data.errors) {
-			return data.errors;
-		}
-	} else {
-		return ["An error occurred. Please try again."];
-	}
-};
-
-export const logout = () => async (dispatch) => {
-	const response = await fetch("/api/auth/logout", {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
-
-	if (response.ok) {
-		dispatch(removeUser());
-	}
-};
-
-export const signUp = (username, email, password) => async (dispatch) => {
-	const response = await fetch("/api/auth/signup", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			username,
-			email,
-			password,
-		}),
-	});
-
-	if (response.ok) {
-		const data = await response.json();
-		dispatch(setUser(data));
-		return null;
-	} else if (response.status < 500) {
-		const data = await response.json();
-		if (data.errors) {
-			return data.errors;
-		}
-	} else {
-		return ["An error occurred. Please try again."];
-	}
-};
-
-export default function reducer(state = initialState, action) {
+export default function karutaReducer(state = initialState, action) {
 	switch (action.type) {
 		case SET_USER:
 			return { user: action.payload };
@@ -104,3 +17,35 @@ export default function reducer(state = initialState, action) {
 			return state;
 	}
 }
+
+// state = {
+// 	0: {
+// 		'author': {
+// 			'english': '',
+// 			'japanese': '',
+// 			'romaji': ''
+// 		},
+// 		'english': {
+// 			0: '',
+// 			1: '',
+// 			2: '',
+// 			3: '',
+// 			4: ''
+// 		},
+// 		'japanese': {
+// 			0: '',
+// 			1: '',
+// 			2: '',
+// 			3: '',
+// 			4: ''
+// 		},
+// 		'romaji': {
+// 			0: '',
+// 			1: '',
+// 			2: '',
+// 			3: '',
+// 			4: ''
+// 		}
+// 	},
+// 	//...
+// }
