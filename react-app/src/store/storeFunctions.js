0b012cb40d3232900455564daa9d3f2e6e44cf66
TheSicilian12 <mguidera314@gmail.com>
2023-07-07 11:30:40 -0400

    time to test karuta normalizer

--- react-app/src/store/storeFunctions.js
+++ react-app/src/store/storeFunctions.js
@@ -14,30 +14,30 @@ export const karutaCardNormalizer = (data) => {
   data.forEach(item => {
     newObj[item.id] = {
       'author': {
-        'english': '',
-        'japanese': '',
-        'romaji': ''
+        'english': item.english_author,
+        'japanese': item.japanese_author,
+        'romaji': item.romaji_author
       },
       'english': {
-        0: '',
-        1: '',
-        2: '',
-        3: '',
-        4: ''
+        0: item.english_line1,
+        1: item.english_line2,
+        2: item.english_line3,
+        3: item.english_line4,
+        4: item.english_line5
       },
       'japanese': {
-        0: '',
-        1: '',
-        2: '',
-        3: '',
-        4: ''
+        0: item.japanese_line1,
+        1: item.japanese_line2,
+        2: item.japanese_line3,
+        3: item.japanese_line4,
+        4: item.japanese_line5
       },
       'romaji': {
-        0: '',
-        1: '',
-        2: '',
-        3: '',
-        4: ''
+        0: item.romaji_line1,
+        1: item.romaji_line2,
+        2: item.romaji_line3,
+        3: item.romaji_line4,
+        4: item.romaji_line5
       }
     }
   })
