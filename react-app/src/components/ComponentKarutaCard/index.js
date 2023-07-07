cd2efe6b24e7e2761d3382c9f5c955cbebb61799
TheSicilian12 <mguidera314@gmail.com>
2023-07-07 11:59:11 -0400

    card component working

--- react-app/src/components/ComponentKarutaCard/index.js
+++ react-app/src/components/ComponentKarutaCard/index.js
@@ -3,12 +3,21 @@ import { useDispatch } from 'react-redux';
 
 import "./ComponentKarutaCard.css";
 
-export default function ComponentKarutaCard(cardData) {
+export default function ComponentKarutaCard({cardData}) {
+
+  if (!cardData) return null
+
+  const {author} = cardData
+
+  // console.log("cardData: ", cardData)
+  // console.log("cardData author: ", author)
 
   return (
-   <div className='karuta-card-container'>
+  <div className='karuta-card-container'>
+    {cardData.english[0]}
+    {cardData.english[1]}
     
 
-    </div>
+  </div>
   );
 }
--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -10,16 +10,20 @@ export default function HomePage() {
   const dispatch = useDispatch();
 
   const cards = useSelector(state => state.karutaCards)
+  console.log("cards: ", cards)
 
   useEffect(() => {
     dispatch(getAllKarutaCardsTHUNK())
   }, [dispatch])
 
+  // console.log("card: ", cards[1].author)
+
+  if (!cards[1]) return null
 
   return (
    <div>
     Hello
-    <ComponentKarutaCard cardData={cards[0]}/>
+    <ComponentKarutaCard cardData={cards[1]} />
     </div>
   );
 }
