570a02a6929cec8d21b38289a33056821149293a
TheSicilian12 <mguidera314@gmail.com>
2023-07-10 10:59:46 -0400

    initial display

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -10,7 +10,7 @@ import { Modal } from "../../context/Modal";
 
 export default function HomePage() {
   const dispatch = useDispatch();
-  const [displayLanguage, setDisplayLanguage] = useState("")
+  const [displayLanguage, setDisplayLanguage] = useState("english")
 
   const cards = useSelector(state => state.karutaCards)
 
@@ -20,25 +20,55 @@ export default function HomePage() {
 
   if (!cards[1]) return null
 
+  const displayCards = (num1, num2) => {
+    console.log(num1, num2)
+  }
+
   return (
    <div>
     <div>
-      <button>All</button>
-      <button>1 - 10</button>
-      <button>11 - 20</button>
-      <button>21 - 30</button>
-      <button>31 - 40</button>
-      <button>41 - 50</button>
-      <button>51 - 60</button>
-      <button>61 - 70</button>
-      <button>71 - 80</button>
-      <button>81 - 90</button>
-      <button>91 - 100</button>
-
+      <button
+        onClick={() => displayCards(1, 100)}
+      >
+        All
+        </button>
+      <button>
+        1 - 10
+        </button>
+      <button>
+        11 - 20
+        </button>
+      <button>
+        21 - 30
+        </button>
+      <button>
+        31 - 40
+        </button>
+      <button>
+        41 - 50
+        </button>
+      <button>
+        51 - 60
+        </button>
+      <button>
+        61 - 70
+        </button>
+      <button>
+        71 - 80
+        </button>
+      <button>
+        81 - 90
+        </button>
+      <button>
+        91 - 100
+        </button>
 
     </div>
     <LanguageToggle displayLanguage={displayLanguage} setDisplayLanguage={setDisplayLanguage} languageOne={"english"} languageTwo={"japanese"} />
-    <ComponentKarutaCard language={displayLanguage} cardData={cards[1]} />
+    {Object.values(cards).map((card) =>
+    <div>
+      <ComponentKarutaCard language={displayLanguage} cardData={card} />
+      </div>)}
     </div>
   );
 }
