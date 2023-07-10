1c129eae7606aab8a746ad1f2ddd5a5bea5bfb86
TheSicilian12 <mguidera314@gmail.com>
2023-07-10 14:28:17 -0400

    working on poem number buttons

--- react-app/src/components/OneHundredPoemsPage/index.js
+++ react-app/src/components/OneHundredPoemsPage/index.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllKarutaCardsTHUNK } from "../../store/karutaCards";
 
 import "./OneHundredPoemsPage.css";
+import "../UniversalCSS.css"
 import ComponentKarutaCard from "../ComponentKarutaCard";
 import ComponentPoem from "../ComponentPoem";
 import LanguageToggle from "../LanguageToggle";
@@ -33,56 +34,67 @@ export default function OneHundredPoemsPage() {
     <div>
       <div>
         <button
+          className="button-basic"
           onClick={() => displayCards(1, 100)}
         >
           All
         </button>
         <button
+          className="button-basic"
           onClick={() => displayCards(1, 10)}
         >
           1 - 10
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(11, 20)}
         >
           11 - 20
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(21, 30)}
         >
           21 - 30
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(31, 40)}
         >
           31 - 40
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(41, 50)}
         >
           41 - 50
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(51, 60)}
         >
           51 - 60
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(61, 70)}
         >
           61 - 70
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(71, 80)}
         >
           71 - 80
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(81, 90)}
         >
           81 - 90
         </button>
         <button
+         className="button-basic"
           onClick={() => displayCards(91, 100)}
         >
           91 - 100
