9dffeae6810ba06f02a10effe2b20ece0aaab7fd
TheSicilian12 <mguidera314@gmail.com>
2023-07-10 15:43:18 -0400

    removing kanjiapi routes not sure yet

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch} from 'react-redux';
 
 import "./Homepage.css";
 
 export default function HomePage() {
   const dispatch = useDispatch();
+
 
   return (
     <div>
