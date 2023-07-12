0d6872d3541dfc3710400a7a0a3fd03a67465a31
TheSicilian12 <mguidera314@gmail.com>
2023-07-12 08:48:03 -0400

    changed tab name and added photos to karuta page

--- react-app/src/components/KarutaPoemPage/index.js
+++ react-app/src/components/KarutaPoemPage/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useDispatch} from 'react-redux';
+import { useParams } from "react-router-dom";
 
 import "./KarutaPoemPage.css";
 
 export default function KarutaPoemPage() {
   const dispatch = useDispatch();
 
+  const {poemId} = useParams()
+  console.log("poemId: ", poemId)
+
   return (
     <div>
     Karuta Poem Page
+    <img src={`https://jti.lib.virginia.edu/japanese/hyakunin/images/onna${poemId}.jpg`} />
     </div>
   );
 }
