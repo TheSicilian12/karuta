92fd7fc391d5583bc49d73e2765ae685f4cc4b31
TheSicilian12 <mguidera314@gmail.com>
2023-07-18 15:38:37 -0400

    added deck cards

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .karuta_cards import seed_karuta_cards, undo_karuta_cards
+from .deck_cards import seed_deck_cards, undo_deck_cards
 
 from app.models.db import db, environment, SCHEMA
 
@@ -16,10 +17,12 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_deck_cards()
         undo_karuta_cards()
         undo_users()
     seed_users()
     seed_karuta_cards()
+    seed_deck_cards()
 
     # Add other seed functions here
 
@@ -27,6 +30,7 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_deck_cards()
     undo_karuta_cards()
     undo_users()
     # Add other undo functions here
