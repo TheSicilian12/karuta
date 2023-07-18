a7ca0448b0f8a0dd4f3aa8cc65d40ff3cc914438
TheSicilian12 <mguidera314@gmail.com>
2023-07-18 14:18:54 -0400

    adding a deck model

---
+++ app/forms/deck_form.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Decks
+
+class DeckForm(FlaskForm):
+    answer = StringField(
+        'answer', validators=[DataRequired()]
+    )
+    answer_long = StringField(
+        'answer_long'
+    )
+    question = StringField(
+        'question', validators=[DataRequired()]
+    )
