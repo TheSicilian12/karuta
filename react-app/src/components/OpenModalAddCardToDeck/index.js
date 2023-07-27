e83ec20be0f293b14634f91781f6c203f7281645
TheSicilian12 <mguidera314@gmail.com>
2023-07-27 15:25:18 -0400

    toggle works

--- react-app/src/components/OpenModalAddCardToDeck/index.js
+++ react-app/src/components/OpenModalAddCardToDeck/index.js
@@ -27,6 +27,11 @@ function OpenModalAddCardToDeck({
 
   const { closeModal } = useModal();
 
+  const [displayCards, setDisplayCards] = useState("all");
+  // all - all deck cards
+  // notDeck - cards not in the deck
+  // deck - cards in the deck
+
   useEffect(() => {
     dispatch(getCardsTHUNK());
   }, [dispatch])
@@ -51,6 +56,20 @@ function OpenModalAddCardToDeck({
       <form
         onSubmit={handleSubmit}>
 
+        <div className="add-card-to-deck-title-selector-container">
+          <h2 className={displayCards === "all" ? `add-card-to-deck-${displayCards}-selection` : ''}
+            onClick={() => setDisplayCards('all')}>
+            All Cards
+          </h2>
+          <h2 className={displayCards === "notDeck" ?  `add-card-to-deck-${displayCards}-selection` : ''}
+            onClick={() => setDisplayCards('notDeck')}>
+            Not in Deck Cards
+          </h2>
+          <h2 className={displayCards === "deck" ?  `add-card-to-deck-${displayCards}-selection` : ''}
+            onClick={() => setDisplayCards('deck')} >
+            Deck Cards
+          </h2>
+        </div>
 
         {/* All cards */}
         <div className="add-card-to-deck-cards-container">
